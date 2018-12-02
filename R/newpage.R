272caa0fc1f23e034380f756461b5106c49581ba
niszet <niszet0016@gmail.com>
2018-12-02 12:38:35 +0900

    update manual

--- R/newpage.R
+++ R/newpage.R
@@ -2,6 +2,8 @@
 #'
 #' Insert new page by adding ooxml code into docx. This function returns the ooxml codes without any parameters. Use inline R chunk insetead of R code block (to add ooxml code directly into sentenses)
 #'
+#' Function returns ooxml code as plain character vector. You can use this output as character vector.
+#'
 newpage <- function() {
   sprintf("%s\n%s\n%s\n",
           '```{=openxml}',
